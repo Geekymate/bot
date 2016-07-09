e3a9164180aad94eb9aa44db774b84fed4dffb27
Andrew Abarykov <expfront@gmail.com>
2016-07-09 23:02:44 +0300

    fix: add new inputs and add submit function

--- src/modules/Test/controller/TestCtrl.js
+++ src/modules/Test/controller/TestCtrl.js
@@ -6,5 +6,48 @@ export default class TestController {
       newTitle: 'Test'
     });
 
+		this.test = {
+			title: 'Тест 1',
+			subject: 'Математика',
+			questions: [{
+				title: '',
+				answers: {
+					answer_one: '',
+					answer_second: '',
+					answer_third: '',
+					answer_fourth: ''
+				},
+				correct_answer: 2
+			}, {
+				title: '',
+				answers: {
+					answer_one: '',
+					answer_second: '',
+					answer_third: '',
+					answer_fourth: ''
+				},
+				correct_answer: 3
+			}, {
+				title: '',
+				answers: {
+					answer_one: '',
+					answer_second: '',
+					answer_third: '',
+					answer_fourth: ''
+				},
+				correct_answer: 1
+			}, {
+				title: '',
+				answers: {
+					answer_one: '',
+					answer_second: '',
+					answer_third: '',
+					answer_fourth: ''
+				},
+				correct_answer: 0
+			}]
+		};
+
+		this.publishTest = () => console.log(this.test);
   }
 }
